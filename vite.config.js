d77ade3b8ebd3e6a429827a4eab6581392220c74
diurno <diurno@max11>
2022-11-17 10:00:35 +0100

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/Breaking-Bad/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
